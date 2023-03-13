d4834b571e1095c2d2ef0a904c2698d63e3084e6
macielsousa <fmacielsousa@gmail.com>
2023-03-13 16:27:32 -0300

    feat: photo, list photo and componetização

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,26 @@
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { NgModule } from '@angular/core';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { RouterModule, Routes } from "@angular/router";
+import { ErrorsModule } from './errors/errors.module';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+
+
+const routes: Routes = [
+    {
+        path: 'user/:userName',
+        component: PhotoListComponent,
+        resolve: {
+            photos: PhotoListResolver
+        }
+    },
+    {path: 'p/add', component: PhotoFormComponent},
+    {path: '**', component: NotFoundComponent},
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [ RouterModule, ErrorsModule ]
+})
+export class AppRoutingModule {}
